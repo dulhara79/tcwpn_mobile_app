5916f0766f2b9b0546fabc444eebd7d2e70b8734
Dulhara Kaushalya <128912391+dulhara79@users.noreply.github.com>
2026-09-16 21:24:15 +0530

    feat: add notification gateway contract

---
+++ lib/core/notifications/attention_notification_gateway.dart
@@ -0,0 +1,6 @@
+abstract interface class AttentionNotificationGateway {
+  Future<void> initialize();
+  Future<bool> requestPermission();
+  Future<void> showAttentionEvent(String eventId);
+  Stream<String> get openedEventIds;
+}
